7efd914dada44deecc11284800806b381f98f121
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2021-04-21 15:37:09 -0300

    [AULA-02] - Finalização da interface base da HomeScreen.

--- lib/application/screens/home/home.screen.dart
+++ lib/application/screens/home/home.screen.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/application/widgets/ScaffoldItems/app_bar.widget.dart';
 import 'package:devquiz/application/widgets/buttons/level_button.widget.dart';
+import 'package:devquiz/application/widgets/cards/quiz_card.widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,27 +17,58 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            LevelButtonWidget(label: "Fácil"),
-            SizedBox(
-              width: 5,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 24,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Fácil"),
+                SizedBox(
+                  width: 5,
+                ),
+                LevelButtonWidget(label: "Médio"),
+                SizedBox(
+                  width: 5,
+                ),
+                LevelButtonWidget(label: "Difícil"),
+                SizedBox(
+                  width: 5,
+                ),
+                LevelButtonWidget(label: "Perito")
+              ],
             ),
-            LevelButtonWidget(label: "Médio"),
-            SizedBox(
-              width: 5,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget()
+                ],
+              ),
             ),
-            LevelButtonWidget(label: "Difícil"),
-            SizedBox(
-              width: 5,
-            ),
-            LevelButtonWidget(label: "Perito")
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
